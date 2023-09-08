d73f110cca930f23a0801146d8f4f12d4726d283
Harshith0112 <141983280+Harshith0112@users.noreply.github.com>
2023-09-08 14:02:00 +0530

    guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,26 +1,45 @@
-function guess {
+#!/bin/bash
+# File: guessinggame.sh
 
-local count=$(ls . | wc -w) 
-local condition=1
-local answ=0
-
-while [[ $condition -eq 1 ]]
-do
-	echo "how many files in this directory?"
-	read answ
-	if [[ $answ -eq $count ]]
-		
-		then condition=0
-		echo "you was correct!"
-	elif [[ $answ -lt $count ]]
-		then
-		echo "wrong! more than that"
-	else
-		echo "wrong! less than that"
+function check # check if guess is too high or too low
+{
+	local files=$(ls -1A|wc -l)
+	# list one file per line (including hidden ones), count the files
+	if [ $1 -gt $files ] # if guess is too high
+		then echo "1"
+	elif [ $1 -lt $files ] # if guess is too low
+		then echo "-1"
+	else #if guess is correct
+		echo "0"
 	fi
-done
-
-
 }
 
-guess
+function start # main part of code
+{
+	echo "How many files are in the current directory?"
+	local flag=2
+
+	while [ $flag -ne 0 ] # till guess is is not correct
+	do
+		read guess
+		if [[ $guess =~ [^0-9] ]] || [ "$guess" = "" ] # if guess is anything other than a whole number
+		then
+			echo "Please enter a whole number in numerical form"
+			let flag=2
+		else #if input is valid
+			let flag=$(check $guess) # check if guess is correct
+		fi
+
+		if [ $flag -eq 1 ] # print messages accordingly
+			then echo "Your guess is too high. Please try again."
+		elif [ $flag -eq -1 ]
+			then echo "Your guess is too low. Please try again."
+		fi
+	done
+
+	if [ $flag -eq 0 ] # congratulatory message
+		then echo "Congratulations! Your guess is correct!"
+	fi
+}
+
+start # start the main part of the program
